2e1ecc400f16ed08ad9777e52c8878c8ee9b36bf
Dancairo-Gomes <76046040+Dancairo-Gomes@users.noreply.github.com>
2022-02-26 01:09:45 -0300

    Delete devops directory

--- devops/delete-k8s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f ./kubernetes/mysql-configmap.yaml
-kubectl delete -f ./kubernetes/mysql-deployment.yaml
-kubectl delete -f ./kubernetes/service-db.yaml
-kubectl delete -f ./kubernetes/api-configmap.yaml
-kubectl delete -f ./kubernetes/webapi-deployment.yaml
-kubectl delete -f ./kubernetes/service-api.yaml
--- devops/deploy-k8s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f ./kubernetes/mysql-configmap.yaml
-kubectl apply -f ./kubernetes/mysql-deployment.yaml
-kubectl apply -f ./kubernetes/service-db.yaml
-sleep 30
-kubectl apply -f ./kubernetes/api-configmap.yaml
-kubectl apply -f ./kubernetes/webapi-deployment.yaml
-kubectl apply -f ./kubernetes/service-api.yaml
